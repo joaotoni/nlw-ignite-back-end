7b11eb4c54db1272d14f65a6c93955fdd9c609ce
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-01-18 20:25:41 -0300

    Creating routes for title and weekDays

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { create } from "domain";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "./lib/prisma";
@@ -5,7 +6,26 @@ import { prisma } from "./lib/prisma";
 export async function appRoutes(app: FastifyInstance) {
   app.get("/habits", async (request) => {
     // itens requisitados para busca de informação: title e weekDays
+    const creatHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
 
-    const { title, weekDays } = request.body;
+    //[0,]
+    const { title, weekDays } = creatHabitBody.parse(request.body);
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
   });
 }
