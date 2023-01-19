a02c0e7e6e1196c1da6bd2cbefb2f16844096a57
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-01-19 00:24:51 -0300

    the second video of nlw-ignite-back-end has already finished]

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { create } from "domain";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
@@ -14,10 +15,12 @@ export async function appRoutes(app: FastifyInstance) {
     //[0,]
     const { title, weekDays } = creatHabitBody.parse(request.body);
 
+    const today = dayjs().startOf("day").toDate();
+
     await prisma.habit.create({
       data: {
         title,
-        created_at: new Date(),
+        created_at: today,
         weekDays: {
           create: weekDays.map((weekDay) => {
             return {
@@ -27,5 +30,46 @@ export async function appRoutes(app: FastifyInstance) {
         },
       },
     });
+  });
+
+  app.get("/day", async (request) => {
+    const getDayParans = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParans.parse(request.query);
+
+    const parseDate = dayjs(date).startOf("day");
+
+    const weekDay = dayjs(date).get("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completeHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+    };
   });
 }
