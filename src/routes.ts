d119bc9bc2cd2b0034df262c01d2b6836be45db2
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-01-19 23:49:43 -0300

    Stoping here for tomorraw       cast)

--- src/routes.ts
+++ src/routes.ts
@@ -72,4 +72,68 @@ export async function appRoutes(app: FastifyInstance) {
       possibleHabits,
     };
   });
+
+  //completar ou não completar um habito
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    //route param => parâmetro de identificação
+
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate(); //para a pessoa completar o habito no dia que ela queria
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      //remover marcação de completo
+    } else {
+      // Completar o Habito
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
+
+  app.get("/summary", async () => {
+    //[retornar uma array com varias informações dentro]
+
+    const summary = await prisma.$queryRaw`
+    Select D.id, D.date,
+    ( 
+      SELECT
+      cast(count(*) as float)
+      FROM day_habits DH
+      WHERE DH.day_id = D.id
+    ) as completed
+      FROM days D
+`;
+    return summary;
+  });
 }
